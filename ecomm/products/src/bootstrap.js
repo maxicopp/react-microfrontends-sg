4979eac670debb6b3cd6c261446c39a66af3e270
Maximiliano Cóppola <maxicopp@outlook.com>
2022-11-02 02:07:30 -0300

    Refactoring products

--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -1,22 +1,35 @@
 import faker from 'faker';
 
-let products = '';
+const mount = (el) => {
+    let products = '';
 
-for (let i = 0; i < 5; i++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`;
-}
+    for (let i = 0; i < 5; i++) {
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`;
+    }
 
-document.querySelector('#dev-products').innerHTML = products;
+    el.innerHTML = products;
+};
 
 // Context/Situation #1
 // We are running this file in development in isolation
 // We are using our local index.html file
 // Which DEFINETLY has an element with an id of 'dev-products'
 // We want to inmediatly render our app into that element
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#dev-products');
+
+    // Assuming our container doesn't have an element
+    // with id 'dev-products'...
+    if (el) {
+        // We are probably running in isolation
+        mount(el);
+    }
+}
 
 // Context/Situation #2
 // We are running this file in development or production
 // through the container app
 // NO GUARANTEE that an element with an id of 'dev-products' exists
 // WE DO NOT WANT try to inmediatly render the app
+export { mount };
